c9df8496bd914eea7fc08e4281804522ca597350
Andrey Bolshakov <abolshakovy@gmail.com>
2022-11-30 14:44:26 +0500

    feature(templates) update template

--- traficlight/settings.py
+++ traficlight/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mptt',
     'tree',
-    'twitter_bootstrap',
     'django_mptt_admin',
 ]
 
@@ -121,7 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
